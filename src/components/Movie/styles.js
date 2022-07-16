e05bf4a1088c12e171e002bfef8b3b208bc61f28
ToniMarkovic <markovictoni10@gmail.com>
2022-07-16 11:50:02 +0200

    movie categories

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,9 +4,29 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
     color: theme.palette.text.primary,
-    textOverflow: 'elipsis', // if string is too long it will add ...
+    textOverflow: 'ellipsis', // if string is too long it will add ...
     width: '230px',
     whiteSpace: 'nowrap',
     overflow: 'hidden',
