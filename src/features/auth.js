28df7fb7a57377735035749329326129d605169f
ToniMarkovic <markovictoni10@gmail.com>
2022-07-18 13:34:16 +0200

    authentication

---
+++ src/features/auth.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: {},
+  isAuthenticated: false,
+  sessionId: '',
+};
+
+const authSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.user = action.payload;
+      // eslint-disable-next-line no-param-reassign
+      state.isAuthenticated = true;
+      // eslint-disable-next-line no-param-reassign
+      state.sessionId = localStorage.getItem('session_id');
+
+      localStorage.setItem('accountId', action.payload.id);
+    },
+  },
+});
+
+export const { setUser } = authSlice.actions;
+
+export default authSlice.reducer;
+
+// Da nemoramo izdvajati state.user u komponenti pa smo to ovdje napravili
+export const userSelector = (state) => state.user;
